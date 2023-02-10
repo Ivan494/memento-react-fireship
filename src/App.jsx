079923b03ea2a2f27957701c525cb0c99e610a8d
Ivan <wuchitho1@hotmail.com>
2023-02-10 15:55:28 +0800

    game ui done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Card from "./components/Card";
+import shuffle from "./utilities/shuffle";
+
+function App() {
+  const [cards, setCards] = useState(shuffle);
+  return (
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card key={id} image={image} selected={false} onClick={() => {}} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
