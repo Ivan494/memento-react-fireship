92008ba81cd576c45b3b01fd516915e8bededd4b
OPHaru <wuchitho1@hotmail.com>
2023-02-12 13:11:01 +0800

    fix header useEffect return

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 
 const Header = ({ handleNewGame, wins }) => {
-    useEffect(() => (document.title = `${wins} wins`), [wins]);
-
+  useEffect(() => {
+    document.title = `${wins} wins`;
+  }, [wins]);
   return (
     <header className="header">
       <h4>{wins} wins</h4>
